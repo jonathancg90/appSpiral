39e150fb8d0a67fa7345f11df23db8a37dbb6b7d
Jonathancg <Jonathancg90@gmail.com>
2013-05-09 02:38:30 -0400

    Progress

--- spiral/apps/sp/views/Brand.py
+++ spiral/apps/sp/views/Brand.py
@@ -9,6 +9,20 @@ from apps.sp.forms.Brand import BrandForm, BrandFiltersForm
 from apps.sp.models.Brand import Brand
 
 
+class BrandListView(SearchFormMixin, ListView):
+    model = Brand
+    template_name = 'panel/brand/brand_list.html'
+    search_form_class = BrandFiltersForm
+    filtering = {
+        'entry_id': SearchFormMixin.ALL,
+        'name': SearchFormMixin.ALL,
+
+    }
+
+    def get_context_data(self, **kwargs):
+        return super(BrandListView, self).get_context_data(**kwargs)
+
+
 class BrandCreateView(CreateView):
     model = Brand
     form_class = BrandForm
@@ -42,20 +56,5 @@ class BrandDeleteView(DeleteView):
         context = super(BrandDeleteView,self).get_context_data(**kwargs)
         context['action'] = 'delete'
         return context
-
-
-class BrandListView(SearchFormMixin, ListView):
-    model = Brand
-    template_name = 'panel/brand/brand_list.html'
-    search_form_class = BrandFiltersForm
-    filtering = {
-        'entry_id': SearchFormMixin.ALL,
-        'name': SearchFormMixin.ALL,
-
-    }
-
-    def get_context_data(self, **kwargs):
-        return super(BrandListView, self).get_context_data(**kwargs)
-
 
 
