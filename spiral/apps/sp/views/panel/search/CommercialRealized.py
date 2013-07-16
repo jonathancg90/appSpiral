5e6181db6a63e48e2caba6797f0e76477c486bda
Jonathancg <Jonathancg90@gmail.com>
2013-07-16 13:53:55 -0400

    Add: queryset

--- spiral/apps/sp/views/panel/search/CommercialRealized.py
+++ spiral/apps/sp/views/panel/search/CommercialRealized.py
@@ -1,19 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
-from django.views.generic import ListView, RedirectView
-from django.http import Http404
-from django.shortcuts import get_object_or_404
-from django.core.urlresolvers import reverse
+from django.views.generic import ListView
 from apps.common.view import  SearchFormMixin
 from apps.common.view import LoginRequiredMixin
 from apps.sp.models.ModelHasCommercial import ModelHasCommercial
-from apps.sp.models.Model import Model
 from apps.sp.forms.ModelHasCommercial import ModelHasCommercialFilterForm
-from apps.sp.forms.Commercial import CommercialFiltersForm
-from apps.sp.models.Commercial import Commercial
-import datetime
-
 
 class CommercialRealizedListView(LoginRequiredMixin, SearchFormMixin, ListView):
     template_name = 'panel/search/commercial_realized.html'
@@ -27,6 +19,12 @@ class CommercialRealizedListView(LoginRequiredMixin, SearchFormMixin, ListView):
         'commercial__brand__entry': SearchFormMixin.ALL,
         'commercial_realized__iexact': SearchFormMixin.ALL,
     }
+    
+    def get_queryset(self):
+        qs = super(CommercialRealizedListView, self).get_queryset()
+        qs = self._set_filter_brand(qs)
+        qs = self._set_filter_entry(qs)
+        return qs
 
     def _set_filter_entry(self, qs):
         entry_id = str(self.request.GET.get('commercial__brand__entry', None))
