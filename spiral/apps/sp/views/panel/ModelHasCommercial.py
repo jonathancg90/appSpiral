d2bc2bf298b92e0e9a5d8b83b794e88cd25201fb
Jonathancg <Jonathancg90@gmail.com>
2013-09-14 15:54:48 -0400

    Validate model - debug toolbar

--- spiral/apps/sp/views/panel/ModelHasCommercial.py
+++ spiral/apps/sp/views/panel/ModelHasCommercial.py
@@ -81,10 +81,6 @@ class ModelHasCommercialListView(LoginRequiredMixin, SearchFormMixin, ListView):
         context = super(ModelHasCommercialListView, self).get_context_data(**kwargs)
         context['model'] = self.model
         context['model_name'] = self.model.get_name_json()
-        if context['model_name'] is None:
-            context['model_name'] = self.model_code
-        else:
-            context['model_name'] = context['model_name'].get('modelo', None)
         return context
 
 
