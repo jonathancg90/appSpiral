3fda270c5bca2d847e2a732fda0b089c30525910
jonathancg90 <jonathancg90@gmail.com>
2014-06-27 11:34:48 -0500

    update model project

--- spiral/apps/sp/models/Project.py
+++ spiral/apps/sp/models/Project.py
@@ -4,11 +4,30 @@ from django.utils.translation import ugettext_lazy as _
 
 class Project(models.Model):
 
-    TYPE_EXTRA = 2
-    TYPE_CASTING = 1
-    CHOICE_TYPE = (
-        (TYPE_EXTRA, _(u'extra')),
-        (TYPE_CASTING, _(u'casting'))
+    LINE_PHOTO = 4
+    LINE_REPRESENTATION = 3
+    LINE_EXTRA = 2
+    LINE_CASTING = 1
+    CHOICE_LINE = (
+        (LINE_EXTRA, _(u'extra')),
+        (LINE_CASTING, _(u'casting')),
+        (LINE_REPRESENTATION, _(u'casting')),
+        (LINE_PHOTO, _(u'casting'))
+    )
+
+    STATUS_STAND_BY = 0
+    STATUS_START = 1
+    STATUS_FINISH = 2
+
+    CHOICE_STATUS = (
+        (STATUS_START, _(u'Iniciado')),
+        (STATUS_FINISH, _(u'Terminado')),
+        (STATUS_STAND_BY, _(u'Stand By'))
+    )
+
+    line_productions = models.SmallIntegerField(
+        choices=CHOICE_LINE,
+        default=LINE_CASTING
     )
 
     project_code = models.CharField(
@@ -16,16 +35,50 @@ class Project(models.Model):
         unique=True,
     )
 
-    project_name = models.CharField(
-        max_length=45
+    commercial = models.ForeignKey(
+        'commercial',
+        verbose_name=_(u'Comercial'),
+        related_name='project_set',
+        null=False,
     )
-    project_type = models.SmallIntegerField(
-        choices=CHOICE_TYPE,
+
+    start_productions = models.DateField (
+        verbose_name=_(u'Inicio de Produccion'),
+        null=False,
     )
+
+    end_productions = models.DateField (
+        verbose_name=_(u'Final de Produccion'),
+        null=False,
+    )
+
+    budget = models.DecimalField(
+        verbose_name=_(u'Presupuesto'),
+        max_digits=10,
+        decimal_places=2
+    )
+
+    budget_cost = models.DecimalField(
+        verbose_name=_(u'Presupuesto de costo'),
+        max_digits=10,
+        decimal_places=2,
+        null=True
+    )
+
+    observations = models.TextField(
+        verbose_name= 'Observaciones'
+    )
+
+    status = models.SmallIntegerField(
+        choices=CHOICE_STATUS,
+        default= STATUS_START
+    )
+
     created = models.DateTimeField(
         auto_now_add=True,
         editable=False
-        )
+    )
+
     modified = models.DateTimeField(
         auto_now_add=True
     )
