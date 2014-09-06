1f620030db9f4273d0ac4b06fda35c36c349fafa
jonathancg90 <jonathancg90@gmail.com>
2014-09-06 08:34:47 -0500

    test

--- spiral/apps/sp/management/migration/Model.py
+++ spiral/apps/sp/management/migration/Model.py
@@ -495,7 +495,6 @@ class ModelProcessMigrate(LoginRequiredMixin, JSONResponseMixin, View):
 
         date_higher = None
         for key in sorted(data_photos, reverse=True):
-            import pdb;pdb.set_trace()
             picture = data_photos.get(key)
             if picture.taken_date is not None:
                 if date_higher is None:
@@ -512,6 +511,7 @@ class ModelProcessMigrate(LoginRequiredMixin, JSONResponseMixin, View):
 
                         main_picture = picture
                         date_higher = picture.taken_date
+        import pdb;pdb.set_trace()
         if main_picture is None and len(data_photos) > 0:
             main_picture = data_photos[0]
 
